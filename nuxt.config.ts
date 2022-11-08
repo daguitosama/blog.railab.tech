3d32444c55f4798775552af7745ec6403ef4ebf1
daguitosama <tocarralero@gmail.com>
2022-11-08 17:07:32 -0500

    change: nuxt update to rc-13 & +@nuxtjs/i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@
 export default defineNuxtConfig({
     
     modules:[
+        '@nuxtjs/i18n',
         '@nuxtjs/tailwindcss'
     ]
+    
 })
--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,6 @@ export default {
 
 <template>
     <div>
-        <h1>Foo bar</h1>
-        <iframe
-            style="border: 1px solid rgba(0, 0, 0, 0.1)"
-            width="800"
-            height="450"
-            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FpVXX2U1TjvpfwLzVOexRW1%2Fblog.railab.tech%3Fpage-id%3D0%253A1%26node-id%3D3%253A248%26viewport%3D25%252C491%252C0.89%26scaling%3Dscale-down"
-            allowfullscreen
-        ></iframe>
+        
     </div>
 </template>
