77e8da377e69f4d8ae6216202a2caefb3c737bed
daguitosama <tocarralero@gmail.com>
2022-11-09 15:09:50 -0500

    temo

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <script>
 export default {
 
-    setup(props) { },
+    setup(props) {
+
+        // 
+
+        // head
+
+    },
 };
 </script>
 
 <template>
-    <div class="bg-[#202124] text-slate-200 w-full min-h-screen p-10">
-        <h1 class="text-4xl ">Home</h1>
+    <div class="bg-white  w-full min-h-screen p-10">
+        <h1 class="text-4xl ">
+            <span>Home </span>
+        </h1>
     </div>
 </template>
