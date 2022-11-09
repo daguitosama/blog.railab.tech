d299bba1937bd0d165e57ae8bae415b1f2594b29
daguitosama <tocarralero@gmail.com>
2022-11-09 15:02:33 -0500

    style: added the Inter font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    css: [
+        /* Fonts */
+        '~/assets/css/fonts.css',
+    ],
     modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss'],
 
     // build:{
