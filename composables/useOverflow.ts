9a2c035574c7c8f6892daf99d54bd78bdbbf9ca9
daguitosama <tocarralero@gmail.com>
2022-11-18 16:03:39 -0500

    feat: overflow handling hook

---
+++ composables/useOverflow.ts
@@ -0,0 +1,36 @@
+/**
+ *
+ * @param {HTMLElement} domElelemtRef
+ * @returns
+ */
+export function useOverflow(domElelemtRef) {
+    // client usage only
+    if (typeof document == 'undefined') {
+        return {
+            // keep same signature to avoid server side
+            // compilation errors
+            // but dont do nothing really
+            hideOverflow() {},
+            showOverflow() {},
+        };
+    }
+    // falback to body element
+    if (!domElelemtRef || !(domElelemtRef instanceof HTMLElement)) {
+        domElelemtRef = document.querySelector('body');
+    }
+
+    return {
+        /**
+         * Applies `overflow: hidden` to the element style
+         */
+        hideOverflow() {
+            domElelemtRef.style.overflow = 'hidden';
+        },
+        /**
+         * Resets the overflow prop of element style
+         */
+        showOverflow() {
+            domElelemtRef.style.overflow = '';
+        },
+    };
+}
