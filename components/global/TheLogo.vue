991eb4b5e39cc2df7f8fa9e70cc895f5a3e849bd
daguitosama <tocarralero@gmail.com>
2022-11-18 21:29:15 -0500

    styles: focus -> focus-visible

--- components/global/TheLogo.vue
+++ components/global/TheLogo.vue
@@ -4,7 +4,7 @@
     <div>
         <NuxtLink
             :to="localePath('/')"
-            class="flex items-baseline focus-visible:ring-2 focus-visible:ring-black focus-visible:outline-none rounded-lg"
+            class="flex items-baseline focus-visible:ring-2 focus-visible:ring-black focus-visible:outline-none focus-visible:ring-offset-4 rounded-lg"
         >
             <span class="text-2xl">Railab</span>
             <span class="ml-[12px] font-extrabold text-sm">Blog</span>
