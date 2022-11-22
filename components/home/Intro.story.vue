92aa534c83785ce8b490334ba52c8643342b27e4
daguitosama <tocarralero@gmail.com>
2022-11-22 02:38:02 -0500

    feat (comp): Home Intro

---
+++ components/home/Intro.story.vue
@@ -0,0 +1,35 @@
+<script setup>
+const state = reactive({
+    heading: 'The Railab Blog',
+    copy: 'Mac care and mantainance tutorials. Buying guides, news, tricks and much more.',
+});
+function initState() {
+    return {
+        heading: 'The Railab Blog',
+        copy: 'Mac care and mantainance tutorials. Buying guides, news, tricks and much more.',
+    };
+}
+</script>
+
+<template>
+    <Story title="HomeIntro">
+        <HomeIntro :heading="state.heading" :copy="state.copy" />
+    </Story>
+</template>
+
+<docs lang="md">
+# Home Intro
+
+This component is the intro on the Home Page to tell new landers what it's the site about in a simple look.
+
+---
+
+## Props:
+
+| Name        | Required | Description                    |
+| ----------- | -------- | ------------------------------ |
+| **headign** | `true`   | The main headign               |
+| **copy**    | `true`   | The copy under the main heding |
+
+---
+</docs>
---
+++ components/home/Intro.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+    heading: {
+        type: String,
+        default: 'Foo',
+    },
+    copy: {
+        type: String,
+        default: 'Foo',
+    },
+});
+</script>
+
+<template>
+    <div class="text-center w-full flex justify-center px-[30px] pt-[120px]">
+        <div class="sm:max-w-[520px] lg:max-w-[580px]">
+            <MainHeadign :text="heading" />
+            <p class="mt-[25px] sm:mt-[35px] text-xl">
+                {{ copy }}
+            </p>
+        </div>
+    </div>
+</template>
