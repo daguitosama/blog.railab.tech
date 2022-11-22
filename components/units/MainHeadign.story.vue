cdd9cc6d76e2b1c4919e064073ee76bab7f89f2b
daguitosama <tocarralero@gmail.com>
2022-11-21 23:19:25 -0500

    feat/comp: Main Headign

---
+++ components/units/MainHeadign.story.vue
@@ -0,0 +1,33 @@
+<script setup>
+import MainHeadign from './MainHeadign.vue';
+const state = reactive({
+    text: 'The Railab Blog',
+});
+
+function initState() {
+    return {
+        text: 'The Railab Blog',
+    };
+}
+</script>
+
+<template>
+    <Story title="Main Headign" icon="lucide:heading-1">
+        <MainHeadign :text="state.text" />
+    </Story>
+</template>
+<docs lang="md">
+# Main Headign
+
+This component it intended for the foremost prominent headign on the site.
+
+---
+
+## Props:
+
+| Name     | Required | Description      |
+| -------- | -------- | ---------------- |
+| **text** | `true`   | The text content |
+
+---
+</docs>
