677ca3379e51f83bc5edd00c0592d152b23a866e
daguitosama <tocarralero@gmail.com>
2022-11-22 01:37:35 -0500

    feat: beskpoke font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,50 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const twColors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme');
+const twColors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
+    content: [],
+    theme: {
+        colors: {
+            // tw colors
+            ...twColors,
 
-    colors: {
-      // tw colors
-      ...twColors,
+            // design colors
+            primary: {
+                on: {
+                    light: {
+                        base: '#2563EB',
+                    },
+                },
+            },
+            type: {
+                on: {
+                    light: {
+                        base: '#292929',
+                        dimmed: '#757575',
+                    },
+                },
+            },
+            light: {
+                base: '#fff',
+                elevation: '#E6E6E6',
+            },
+            dark: {
+                base: '#1D1F22', // apple dark mode solid bg
+                // google dark mode bg (nice to)
+                // 202124
+            },
+        },
 
-      // design colors
-      primary: {
-        on: {
-          light: {
-            base: '#2563EB'
-          }
-        }
-      },
-      type: {
-        on: {
-          light: {
-            base: '#292929',
-            dimmed: '#757575'
-          }
-        }
-      },
-      light: {
-        base: '#fff',
-        elevation: '#E6E6E6'
-      },
-      dark: {
-        base: '#1D1F22' // apple dark mode solid bg
-        // google dark mode bg (nice to) 
-        // 202124
-      }
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                bespoke: ['BespokeSerif var', ...defaultTheme.fontFamily.sans],
+                //
+                // mono: ['Fira Code VF', ...defaultTheme.fontFamily.mono],
+                // source: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-
-
-    extend: {
-
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-        // mono: ['Fira Code VF', ...defaultTheme.fontFamily.mono],
-        // source: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
-      },
-    },
-
-  },
-  plugins: [],
-}
+    plugins: [],
+};
