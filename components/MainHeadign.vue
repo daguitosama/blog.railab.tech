d8c0ad31f3196f5a68e33cb36afabfd44a7b00ac
daguitosama <tocarralero@gmail.com>
2022-11-22 02:00:20 -0500

    feat (comp): Main Headign

---
+++ components/MainHeadign.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+    /**
+     * The text content
+     */
+    text: {
+        type: String,
+        default: 'Supper Nice Headign',
+    },
+
+    /**
+     * Id in case needed for a fragment identifier
+     *
+     */
+    // id: {
+    //     type: String,
+    // },
+});
+</script>
+
+<template>
+    <h1
+        class="font-bespoke text-type-on-light-base font-bold pb-2 text-4xl sm:text-5xl md:text-6xl"
+    >
+        {{ text }}
+    </h1>
+</template>
