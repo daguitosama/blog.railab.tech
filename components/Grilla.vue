1e8398c64c6a5c108bf7e9a2942d6192d8d2480e
daguitosama <tocarralero@gmail.com>
2022-11-24 15:43:17 -0500

    dev: grilla to see distribution

---
+++ components/Grilla.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+    prop1: {},
+});
+</script>
+
+<template>
+    <div class="fixed inset-0 w-full h-full grid grid-cols-2 grid-rows-2 z-10">
+        <div
+            class="border-r border-b border-black flex items-center justify-center text-rose-500 font-bold text-3xl"
+        >
+            1
+        </div>
+        <div
+            class="border-b border-black flex items-center justify-center text-rose-500 font-bold text-3xl"
+        >
+            2
+        </div>
+        <div
+            class="border-r border-black flex items-center justify-center text-rose-500 font-bold text-3xl"
+        >
+            3
+        </div>
+        <div
+            class="flex items-center justify-center text-rose-500 font-bold text-3xl"
+        >
+            4
+        </div>
+    </div>
+</template>
