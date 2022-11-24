6d971c74a291a0fcec5886a72690ff6d15d23dd1
daguitosama <tocarralero@gmail.com>
2022-11-24 14:43:48 -0500

    styles: dark mode

--- components/global/NavigationMobileMenuLink.vue
+++ components/global/NavigationMobileMenuLink.vue
@@ -23,8 +23,11 @@ const props = defineProps({
         <NuxtLink
             :to="localePath(link.route)"
             :tabindex="isReachable ? 0 : -1"
-            class="font-bespoke focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-4 focus-visible:ring-black rounded-lg text-type-on-light-base hover:text-primary-on-light-base hover:underline active:text-primary-on-light-base"
-            :class="{ 'underline text-primary-on-light-base': isCurrent }"
+            class="font-bespoke focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-4 focus-visible:ring-black rounded-lg text-type-on-light-base dark:text-type-on-dark-base hover:text-primary-on-light-base hover:underline active:text-primary-on-light-base"
+            :class="{
+                'underline text-primary-on-light-base dark:text-primary-on-light-base':
+                    isCurrent,
+            }"
         >
             {{ link.text }}</NuxtLink
         >
