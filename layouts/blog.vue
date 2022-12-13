d21825a996f66bd203a5833893a40f6f8a4d3691
daguitosama <tocarralero@gmail.com>
2022-12-13 13:32:08 -0500

    prepare to use metadata from storyblok

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -16,13 +16,24 @@ const canonical = computed(
     () => head.value?.link.filter((link) => link.rel == 'canonical')[0].href
 );
 
-// Page Metadata
-// TEST page metadata harcoded success
-usePageMetadata({ title: 'awesome title', description: 'awesome description', image_url: 'awesome image url', canonical_url: 'supper canonical' })
 
-// 
+
+// TODO hidrate page metadata with real content from ST api
+//  make the content gets a `meta_data` component as ST respective blok
+// use that `meta_data` story to create the page metadata
 const content = useContent();
 
+
+// Page Metadata
+// TEST page metadata harcoded success
+usePageMetadata({
+    title: 'awesome title',
+    description: 'awesome description',
+    image_url: 'awesome image url',
+    canonical_url: canonical,
+});
+
+//
 
 
 function onError(error) {
@@ -41,27 +52,12 @@ function onError(error) {
             <template v-for="meta in head.meta" :key="meta.id">
                 <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
             </template>
-
-            <!-- page metadata -->
-            <!-- base -->
-            <!-- <Title> {{ content.page_meta_data.title }} </Title>
-            <Meta name="description" :content="content.page_meta_data.description" /> -->
-            <!-- OG -->
-            <!-- <Meta property="og:title" :content="content.page_meta_data.title" />
-            <Meta property="og:description" :content="content.page_meta_data.description" />
-            <Meta property="og:image" :content="content.page_meta_data.image" />
-            <Meta property="og:site_name" :content="siteName" /> -->
-            <!-- use canonical -->
-            <!-- <Meta property="og:url" :content="canonical" /> -->
-            <!-- Twitter -->
-            <!-- <Meta property="twitter:card" content="summary_large_image" />
-            <Meta property="twitter:title" :content="content.page_meta_data.title" />
-            <Meta property="twitter:image" :content="content.page_meta_data.image" />
-            <Meta property="twitter:site" :content="twitterSite" /> -->
         </Head>
 
-        <Body
-            class="antialiased bg-surface-light-base dark:bg-surface-dark-base text-type-on-light-base dark:text-type-on-dark-base transition-all duration-300">
+        <Body class="
+             antialiased bg-surface-light-base dark:bg-surface-dark-base text-type-on-light-base 
+             dark:text-type-on-dark-base transition-all duration-300
+            ">
             <TheNavigationProvider />
 
             <slot />
